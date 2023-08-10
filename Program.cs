b8585aac395b83a44d6e3aae69e69e0eb8e7b6df
beyzaaksu <79789978+beyzaaksu@users.noreply.github.com>
2023-08-10 16:57:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+    Console.WriteLine("Bir sayi giriniz");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Girdiğiniz sayi:" + sayi);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Hata:" + ex.Message.ToString());
+}
+finally
+{
+    Console.WriteLine("İşlem Tamamlandı!");
+}
+
+try
+{
+    int a = int.Parse("-200000000000000");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine(ex);
+    Console.WriteLine("Null değer girdiniz");
+
+}
+catch (FormatException ex)
+{
+    Console.WriteLine(ex);
+    Console.WriteLine("Veri tipi uyuşmuyor!");
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine(ex);
+    Console.WriteLine("Çok büyük ya da çok küçük değer girdiniz!");
+}
+
+finally
+{
+    Console.WriteLine("İşlem başarıyla tamamlandı!");
+}
